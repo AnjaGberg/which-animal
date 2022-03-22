7f36fae4d2cd32104bef1742a964de511867186c
Anja Greifenberg <anja.gberg@gmx.de>
2022-03-22 12:29:41 +0100

    connected stimulus controller to game

--- app/javascript/controllers/tiles_controller.js
+++ app/javascript/controllers/tiles_controller.js
@@ -2,15 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("The 'check_all_checkboxes' controller is now loaded!")
+    console.log("The 'tiles' controller is now loaded!")
   }
-  
-  noDisplayTileOnClick = () => {
-   const tile = document.querySelector('tile');
-   tile.addEventListener('click', (event) => {
-     element.style.display = "none"
-   });
-  };
 
-  noDisplayTileOnClick();
+  // noDisplayTileOnClick = () => {
+  //  const tile = document.querySelector('tile');
+  //  tile.addEventListener('click', (event) => {
+  //    element.style.display = "none"
+  //  });
+  // };
+
+  // noDisplayTileOnClick();
 }
