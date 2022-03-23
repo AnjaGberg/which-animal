7dcb0d84762b7618282f2fc375c1fcbdc68834f3
Anja Greifenberg <anja.gberg@gmx.de>
2022-03-23 15:44:27 +0100

    adding controller actions and controller targets

--- app/javascript/controllers/tiles_controller.js
+++ app/javascript/controllers/tiles_controller.js
@@ -1,16 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["tile"]
+
   connect() {
     console.log("The 'tiles' controller is now loaded!")
   }
 
-  // noDisplayTileOnClick = () => {
-  //  const tile = document.querySelector('tile');
-  //  tile.addEventListener('click', (event) => {
-  //    element.style.display = "none"
-  //  });
-  // };
-
-  // noDisplayTileOnClick();
+  hide() {
+    this.tileTarget.classList.add("d-none");
+  }
 }
