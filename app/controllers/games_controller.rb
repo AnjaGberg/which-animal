2c4f1325262bd7388c555463a34fd84bd6152b3e
Anja Greifenberg <anja.gberg@gmx.de>
2022-03-25 13:43:20 +0100

    form broken

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,8 @@ class GamesController < ApplicationController
   def new
     @animal = Animal.all.sample
   end
+
+  def score
+    @answer = params[:answer]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root "games#new"
 
   resources :games, only: [:new]
+  post '/', to: 'games#score'
+  post 'score', to: 'games#score'
 end
